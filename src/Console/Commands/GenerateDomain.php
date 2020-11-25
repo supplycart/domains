18e1b737995e19aef6ccef7ff10c0fb9b6130c37
scaliaabel <scaliaabel@supplycart.my>
2020-11-25 13:03:24 +0800

    Add Unit Test and Populate Files

--- src/Console/Commands/GenerateDomain.php
+++ src/Console/Commands/GenerateDomain.php
@@ -15,11 +15,74 @@ class GenerateDomain extends Command
     public $name;
 
     /**
+     * Domain Path.
+     *
+     * @var
+     */
+    public $domainPath;
+
+    /**
+    * Model Path.
+    *
+    * @var
+    */
+    public $modelPath;
+
+    /**
+    * Http Path.
+    *
+    * @var
+    */
+    public $httpPath;
+
+    /**
+    * Listener Path.
+    *
+    * @var
+    */
+    public $listenerPath;
+
+    /**
+    * Event Path.
+    *
+    * @var
+    */
+    public $eventPath;
+
+    /**
+    * Job Path.
+    *
+    * @var
+    */
+    public $jobPath;
+
+    /**
+    * Contract Path.
+    *
+    * @var
+    */
+    public $contractPath;
+
+    /**
+    * Policy Path.
+    *
+    * @var
+    */
+    public $policyPath;
+
+    /**
+    * Controller Path.
+    *
+    * @var
+    */
+    public $controllerPath;
+
+    /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'make:domain {name : Domain Name}';
+    protected $signature = 'generate:domain {name : Domain Name}';
 
     /**
      * The console command description.
@@ -47,27 +110,109 @@ class GenerateDomain extends Command
     {
         $this->name = ucfirst($this->argument('name'));
 
+        $this->domainPath = app_path('Domains' . DIRECTORY_SEPARATOR . $this->name);
+
+        $this->modelPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Models';
+
+        $this->httpPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Http';
+       
+        $this->jobPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Jobs';
+
+        $this->listenerPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Listeners';
+
+        $this->policyPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Policies';
+
+        $this->eventPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Event';
+
+        $this->contractPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Contracts';
+
+        $this->controllerPath = $this->httpPath . DIRECTORY_SEPARATOR . 'Controllers';
+
         $this->generateDirectories();
 
-        return 1;
+        $this->generateFiles();
+
+        return 'Folder generated';
     }
 
     public function generateDirectories()
     {
-        if (!File::exists(app_path('Domains'))) {
+        if (!File::exists($this->domainPath)) {
             File::makeDirectory($this->domainPath, 0777, true);
         }
 
-        File::makeDirectory(app_path('Domains' . DIRECTORY_SEPARATOR . $this->name, 0777, true));
+        File::makeDirectory($this->modelPath, 0777, true);
 
-        File::makeDirectory(app_path('Domains' . DIRECTORY_SEPARATOR . $this->name . DIRECTORY_SEPARATOR . 'Models'));
+        File::makeDirectory($this->httpPath, 0777, true);
 
-        File::makeDirectory(app_path('Domains' . DIRECTORY_SEPARATOR . $this->name . DIRECTORY_SEPARATOR . 'Http'));
+        File::makeDirectory($this->listenerPath, 0777, true);
 
-        File::makeDirectory(app_path('Domains' . DIRECTORY_SEPARATOR . $this->name . DIRECTORY_SEPARATOR . 'Listeners'));
+        File::makeDirectory($this->contractPath, 0777, true);
 
-        File::makeDirectory(app_path('Domains' . DIRECTORY_SEPARATOR . $this->name . DIRECTORY_SEPARATOR . 'Contracts'));
+        File::makeDirectory($this->eventPath, 0777, true);
 
-        File::makeDirectory(app_path('Domains' . DIRECTORY_SEPARATOR . $this->name . DIRECTORY_SEPARATOR . 'Events'));
+        File::makeDirectory($this->jobPath, 0777, true);
+
+        File::makeDirectory($this->httpPath . DIRECTORY_SEPARATOR . 'Controllers', 0777, true);
+
+        File::makeDirectory($this->policyPath, 0777, true);
+    }
+
+    public function generateFiles()
+    {
+        file_put_contents(
+            $this->domainPath . DIRECTORY_SEPARATOR . "{$this->name}.php",
+            $this->prepareFile(file_get_contents(__DIR__ . '/../../stubs/domain.stub'))
+        );
+
+        file_put_contents(
+            $this->modelPath . DIRECTORY_SEPARATOR . "{$this->name}.php",
+            $this->prepareFile(file_get_contents(__DIR__ . '/../../stubs/model.stub'))
+        );
+
+        file_put_contents(
+            $this->listenerPath . DIRECTORY_SEPARATOR . "{$this->name}Listener.php",
+            $this->prepareFile(file_get_contents(__DIR__ . '/../../stubs/listener.stub'))
+        );
+
+        file_put_contents(
+            $this->eventPath . DIRECTORY_SEPARATOR . "{$this->name}Event.php",
+            $this->prepareFile(file_get_contents(__DIR__ . '/../../stubs/event.stub'))
+        );
+
+        file_put_contents(
+            $this->jobPath . DIRECTORY_SEPARATOR . "{$this->name}Job.php",
+            $this->prepareFile(file_get_contents(__DIR__ . '/../../stubs/job.stub'))
+        );
+
+        file_put_contents(
+            $this->policyPath . DIRECTORY_SEPARATOR . "{$this->name}Policy.php",
+            $this->prepareFile(file_get_contents(__DIR__ . '/../../stubs/policy.stub'))
+        );
+
+        file_put_contents(
+            $this->httpPath . DIRECTORY_SEPARATOR . "routes.php",
+            $this->prepareFile(file_get_contents(__DIR__ . '/../../stubs/routes.stub'))
+        );
+
+        file_put_contents(
+            $this->httpPath . DIRECTORY_SEPARATOR . 'Controllers' . DIRECTORY_SEPARATOR . "{$this->name}Controller.php",
+            $this->prepareFile(file_get_contents(__DIR__ . '/../../stubs/controller.stub'))
+        );
+    }
+
+    public function prepareFile($fileContents)
+    {
+        $replacings = [
+            '{{name}}',
+            '{{namespace}}',
+        ];
+
+        $replacements = [
+            $this->name,
+            $this->domainPath
+        ];
+
+        return str_replace($replacings, $replacements, $fileContents);
     }
 }
