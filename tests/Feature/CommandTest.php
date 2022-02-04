9cb91c802d01e5da0c8ca40c2974a423fcb4b07e
scaabel <bidayuhcoder@protonmail.com>
2022-02-04 22:49:02 +0800

    test: add generate queues directories and files test

--- tests/Feature/CommandTest.php
+++ tests/Feature/CommandTest.php
@@ -2,22 +2,29 @@
 
 namespace Supplycart\Domains\Tests\Feature;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 use Supplycart\Domains\Tests\TestCase;
 
 class CommandTest extends TestCase
 {
-    public function test_can_populate_domain()
+	public ?string $domainPath;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		$this->domainPath = app_path('Domains' . DIRECTORY_SEPARATOR . 'Test');
+	}
+
+	public function test_can_populate_domain(): void
     {
-        $this->withoutExceptionHandling();
+        $modelPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Models';
+        $domainModelPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Test.php';
 
-        $domainPath = app_path('Domains' . DIRECTORY_SEPARATOR . 'Test');
-        $modelPath = $domainPath . DIRECTORY_SEPARATOR . 'Models';
-        $domainModelPath = $domainPath . DIRECTORY_SEPARATOR . 'Test.php';
+        $this->artisan('make:domain', ['name' => 'test', '--queues']);
 
-        $this->artisan('make:domain', ['name' => 'test']);
-
-        $this->assertTrue(File::exists($domainPath));
+        $this->assertTrue(File::exists($this->domainPath));
         $this->assertTrue(File::exists($modelPath));
         $this->assertTrue(File::exists($domainModelPath));
 
@@ -36,4 +43,24 @@ class CommandTest extends TestCase
 
         $this->assertEquals($expectedContents, file_get_contents($domainModelPath));
     }
+
+	public function test_can_populate_queues_path(): void
+	{
+		$domainListenersDirectoryPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Listeners';
+		$domainListenersPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Listeners' . DIRECTORY_SEPARATOR . 'TestListener.php';
+		$domainEventsDirectoryPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Events';
+		$domainEventsPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Events' . DIRECTORY_SEPARATOR . 'TestEvent.php';
+		$domainJobsDirectoryPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Jobs';
+		$domainJobsPath = $this->domainPath . DIRECTORY_SEPARATOR . 'Jobs' . DIRECTORY_SEPARATOR . 'TestJob.php';
+
+		Artisan::call('make:domain test --queues');
+
+		$this->assertTrue(File::exists($this->domainPath));
+		$this->assertTrue(File::isDirectory($domainListenersDirectoryPath));
+		$this->assertTrue(File::isDirectory($domainEventsDirectoryPath));
+		$this->assertTrue(File::isDirectory($domainJobsDirectoryPath));
+		$this->assertTrue(File::exists($domainListenersPath));
+		$this->assertTrue(File::exists($domainEventsPath));
+		$this->assertTrue(File::exists($domainJobsPath));
+	}
 }
